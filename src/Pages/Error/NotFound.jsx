b953e7fd979c46014b68efb5507613b5bd84b68d
nasimrifat101 <nasimrifat101@gmail.com>
2024-01-01 15:32:15 +0600

    error element added

--- src/Pages/Error/NotFound.jsx
+++ src/Pages/Error/NotFound.jsx
@@ -1,9 +1,25 @@
+import { Helmet } from "react-helmet-async";
+import { FaBackward } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <Helmet>
+            <title>Sheikh Properties - 404 Not Found</title>
+        </Helmet>
+            <div className="hero min-h-screen flex items-center justify-center">
+                <div className="hero-content text-center">
+                    <div className="">
+                        <h1 className="text-[300px] font-black">404</h1>
+                        <p className="-mt-10 text-xl">Page Not Found</p>
+                        <div className="mt-10 flex justify-center">
+                            <Link to='/' className="btn"><FaBackward /><span>Go Home</span></Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/Providers/Router.jsx
+++ src/Providers/Router.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
+import NotFound from "../Pages/Error/NotFound";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Main />,
+        errorElement:<NotFound/>,
         children: [
             {
                 path: "/",
