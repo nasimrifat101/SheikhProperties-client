9ecdc265ba28c9d0283cf204e7bdd58c89e23c85
nasimrifat101 <nasimrifat101@gmail.com>
2024-03-13 11:55:10 +0600

    [Fix] Login and signup page ui fixxed

--- src/Pages/Home/Components/All/Featured.jsx
+++ src/Pages/Home/Components/All/Featured.jsx
@@ -16,19 +16,20 @@ const Featured = () => {
 
     const datas = data.slice(0, 8)
     return (
-        <div className="max-w-screen-2xl mx-auto min-h-screen my-5">
-            <h1 className="text-4xl font-bold mt-14">Featured Properties</h1>
-            <div className="grid grid-cols-4 gap-4 py-10">
-                {
-                    datas?.map(item => <FeatureCard key={item.id} item={item} />
+        <div className="bg-gradient-to-tr from-[#ffffff] via-[#fffbf3] to-[#ffffff]">
+            <div className="max-w-screen-2xl  mx-auto min-h-screen py-5">
+                <h1 className="text-4xl font-bold pt-14">Featured Properties</h1>
+                <div className="grid grid-cols-4 gap-4 py-10">
+                    {
+                        datas?.map(item => <FeatureCard key={item.id} item={item} />
 
-                    )
-                }
+                        )
+                    }
+                </div>
+                <div className="flex justify-center">
+                    <Link className="font-bold underline btn-ghost btn">Show More Properties</Link>
+                </div>
             </div>
-            <div className="flex justify-center">
-                <Link className="font-bold underline btn-ghost btn">Show More Properties</Link>
-            </div>
-
         </div>
     );
 };
