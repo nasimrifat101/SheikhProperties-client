4e41cf55c28e9ed494f284d5afd4f357e00c81b6
nasimrifat101 <nasimrifat101@gmail.com>
2024-04-08 21:35:06 +0600

    Created login providers

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,10 +1,52 @@
 /* eslint-disable react/prop-types */
-import { createContext } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../Firebase/firebase.config";
 
 export const AuthContext = createContext(null);
+const provider = GoogleAuthProvider()
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('user logged in', currentUser)
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+
+    const loginUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleSignin = () => {
+        return googleSignin(auth, provider)
+    }
+
+
+    const info = {
+        user,
+        createUser,
+        loading,
+        loginUser,
+        googleSignin
+    }
+
+    
     return (
-        <AuthContext.Provider >
+        <AuthContext.Provider value={info}>
             {children}
         </AuthContext.Provider>
     );
